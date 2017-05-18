e8973434066696a441b703447cc321e716e75fe5
Christi Viets <christine.viets@verge-solutions.com>
2017-05-18 14:44:29 -0400

    #7 Support binary mode.

--- lib/mountebank/stub/http_response.rb
+++ lib/mountebank/stub/http_response.rb
@@ -1,9 +1,10 @@
 class Mountebank::Stub::HttpResponse < Mountebank::Stub::Response
-  def self.create(statusCode=200, headers={}, body='', behaviors={})
+  def self.create(statusCode=200, headers={}, body='', behaviors={}, mode='')
     payload = {}
     payload[:statusCode] = statusCode
     payload[:headers] = headers unless headers.empty?
     payload[:body] = body unless body.empty?
+    payload[:_mode] = mode unless mode.empty?
 
     data = {is: payload}
     data.merge!(_behaviors: behaviors) unless behaviors.empty?
--- spec/mountebank/stub/http_response_spec.rb
+++ spec/mountebank/stub/http_response_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe Mountebank::Stub::HttpResponse do
   let(:statusCode) { 200 }
   let(:headers) { {"Content-Type" => "application/json"} }
   let(:body) { {foo:"bar"}.to_json }
-  let!(:response) { Mountebank::Stub::HttpResponse.create(statusCode, headers, body) }
-  let!(:response_with_behaviors) { Mountebank::Stub::HttpResponse.create(statusCode, headers, body, {wait: 10000}) }
+  let(:behaviors) { {} }
+  let(:mode) { '' }
+  let!(:response) { Mountebank::Stub::HttpResponse.create(statusCode, headers, body, behaviors, mode) }
 
   describe '.create' do
     it 'returns response object' do
@@ -13,9 +14,20 @@ RSpec.describe Mountebank::Stub::HttpResponse do
       expect(response.to_json).to eq '{"is":{"statusCode":200,"headers":{"Content-Type":"application/json"},"body":"{\"foo\":\"bar\"}"}}'
     end
 
-    it 'returns a response object' do
-      expect(response_with_behaviors).to be_a Mountebank::Stub::HttpResponse
-      expect(response_with_behaviors.to_json).to eq '{"is":{"statusCode":200,"headers":{"Content-Type":"application/json"},"body":"{\"foo\":\"bar\"}"},"_behaviors":{"wait":10000}}'
+    context 'binary mode' do
+      let(:mode) { 'binary' }
+      it 'returns response object with binary mode' do
+        expect(response).to be_a Mountebank::Stub::HttpResponse
+        expect(response.to_json).to eq '{"is":{"statusCode":200,"headers":{"Content-Type":"application/json"},"body":"{\"foo\":\"bar\"}","_mode":"binary"}}'
+      end
+    end
+
+    context 'with behaviors' do
+        let(:behaviors) { { wait: 10000 } }
+      it 'returns a response object' do
+        expect(response).to be_a Mountebank::Stub::HttpResponse
+        expect(response.to_json).to eq '{"is":{"statusCode":200,"headers":{"Content-Type":"application/json"},"body":"{\"foo\":\"bar\"}"},"_behaviors":{"wait":10000}}'
+      end
     end
   end
 end
